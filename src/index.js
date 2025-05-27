1eb840fd57d33d8a408907da03f0231de2b9a2e8
Bettsy Garces <89868325+bettsygarbur@users.noreply.github.com>
2025-05-27 07:28:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import localeEsMessages from "./locales/es.json";
+import localeEnMessages from "./locales/en.json";
+
+const locale = navigator.language.startsWith('es') ? 'es-ES' : 'en-US';
+const messages = locale.startsWith('es') ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages}>
+    <JobsList locale={locale} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
