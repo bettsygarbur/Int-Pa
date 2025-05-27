ef5ed61f4eaac09cf53330a14dba74d9ff65dbc7
Bettsy Garces <89868325+bettsygarbur@users.noreply.github.com>
2025-05-27 07:28:06 -0500

    Update job.js

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { FormattedDate, FormattedMessage } from 'react-intl';
+
+const formatSalary = (salary, locale) => {
+
+  const formattedNumber = new Intl.NumberFormat(locale).format(salary);
+
+  if (locale.startsWith('es')) {
+
+    if (salary === 1) return `${formattedNumber} millón`;
+    else return `${formattedNumber} millones`;
+  } else {
+
+    return `${formattedNumber} million`;
+  }
+};
 
 const Job = (props) => {
+  const { offer, locale } = props;
+
+
+  const formattedVisits = new Intl.NumberFormat(locale).format(offer.visits);
+
   return (
     <tr>
-      <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <th scope="row">{offer.id}</th>
+      <td>{offer.name}</td>
+      <td>{offer.company}</td>
+      <td>{formatSalary(offer.salary, locale)}</td>
+      <td>{offer.city}</td>
+      <td>
+        <FormattedDate
+          value={new Date(offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+
+        />
+      </td>
+      <td>{formattedVisits}</td>
     </tr>
   );
 };
